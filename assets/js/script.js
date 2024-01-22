6e81c6b57a3ae8d6b743aa5c64c110eb705b8885
steve whitehouse <stewhites@blueyonder.co.uk>
2024-01-22 15:19:29 +0000

    set up functions for game

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractionnQuestion(){
+    
+}
+
+function displayMultiplyQuestion(){
+    
+}
+
+function displayDivideQuestion(){
+    
+}
