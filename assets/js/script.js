7f11463e34e2b74a9c15e2a71a277ed26029e16b
steve whitehouse <stewhites@blueyonder.co.uk>
2024-01-22 15:57:31 +0000

    add code to generate two random numbers for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
-function runGame(){
+//wait for page load before starting game
+//Get button eleemnts and add event listeners to them
+document.addEventListener("DOMContentLoaded",function(){
+    let buttons = document.getElementsByTagName("button");;
 
+    for (let button of buttons){
+        button.addEventListener ("click", function(){
+            if (this.getAttribute("data-type")==="submit"){
+                alert("You Clicked Submit");
+            } else {
+                let gameType= this.getAttribute("data-type");
+                alert (`Yuo clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+/**
+ * The main game loop, called when script is first loaded
+ * and after users answer has been processed
+ */
+function runGame(){
+    //create 2 random numbers 1>25
+    let num1 =Math.floor(Math.random()*25)+1
+    let num2 = Math.floor(Math.random()*25)+1
 }
 
 function checkAnswer(){
