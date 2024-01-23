f3932ff964dd54cef9a5f1bf798de86fded2ac4b
steve whitehouse <stewhites@blueyonder.co.uk>
2024-01-23 16:14:42 +0000

    add multiplication code

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,29 +20,33 @@ document.addEventListener("DOMContentLoaded",function(){
  * The main game loop, called when script is first loaded
  * and after users answer has been processed
  */
-function runGame(gameType){
+function runGame(gameType) {
     //create 2 random numbers 1>25
-    let num1 =Math.floor(Math.random()*25)+1
-    let num2 = Math.floor(Math.random()*25)+1
-    if(gameType==="addition"){
-        displayAdditionQuestion(num1,num2);
-
-    } else{alert(`Unknown Gametype ${gameType}`);
-    throw `Unknown Game Type ${gameType}. Aborting!`
-}
+    let num1 = Math.floor(Math.random() * 25) + 1
+    let num2 = Math.floor(Math.random() * 25) + 1
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else {
+        alert(`Unknown Gametype ${gameType}`);
+        throw `Unknown Game Type ${gameType}. Aborting!`
+    }
 }
 /**
- * checks tha anser against 
+ * checks tha answer against 
  * the first element in the 
  * returned calculatedCorrectAnswer array
  */
 function checkAnswer(){
-let userAnswer = parseInt(document.getElementById("answer-box").value);
-let calculatedAnswer = calculateCorrectAnswer();
-let isCorrect = userAnswer === calculatedAnswer[0];
-if (isCorrect) { alert (" Hey you got it right!")
-} else { 
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    if (isCorrect) { alert (" Hey you got it right!");
+    incrementScore();
+    } else { 
     alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+incrementWrongAnswer();
 }
 runGame (calculatedAnswer[1]);
 }
@@ -50,21 +54,33 @@ runGame (calculatedAnswer[1]);
  *  directly for the DOM and 
  * calculates correct answer */
 function calculateCorrectAnswer(){
-let operand1 = parseInt (document.getElementById('operand1').innerText);
-let operand2 = parseInt (document.getElementById('operand2').innerText);
-let operator = document.getElementById('operator').innerText;
-if (operator === "+") {
+    let operand1 = parseInt (document.getElementById('operand1').innerText);
+    let operand2 = parseInt (document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+    if (operator === "+") {
      return [operand1 + operand2, "addition"];
-    } else { alert ('Unimplemented operator ${operator}');
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];}
+     else { alert ('Unimplemented operator ${operator}');
             throw 'Unimplenented operator ${operator}. Aborting!';
 }
 }
-function incrementScore(){
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
-function incrementWrongAnswer(){
-
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
 }
 
 function displayAdditionQuestion(operand1,operand2){
@@ -78,10 +94,15 @@ function displaySubtractionnQuestion(){
     
 }
 
-function displayMultiplyQuestion(){
-    
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent=operand1;
+    document.getElementById('operand2').textContent=operand2;
+    document.getElementById('operator').textContent="x";
 }
 
 function displayDivideQuestion(){
-    
+    document.getElementById('operand1').textContent=operand1;
+    document.getElementById('operand2').textContent=operand2;
+    document.getElementById('operator').textContent="/";
+ 
 }
